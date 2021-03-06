02bd8c47ee5b9156ecbdd72d891e5bfd0f18cb92
Timothy <exzos_graalonline@hotmail.com>
2021-03-06 11:39:30 -0500

    made album crud layers throw exceptions to service

--- implementation/src/main/java/AlbumManagerInDB.java
+++ implementation/src/main/java/AlbumManagerInDB.java
@@ -3,23 +3,19 @@ import repository.core.Album;
 import repository.core.IAlbumManager;
 import repository.core.RepException;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class AlbumManagerInDB implements IAlbumManager {
-    public ArrayList<Album> listAlbum() {
+    public ArrayList<Album> listAlbum() throws SQLException {
         return AlbumDAO.getAllAlbums();
     }
 
-    public Album getAlbum(String isrc) throws RepException {
-        Album album = AlbumDAO.getAlbumDatabase(isrc);
-
-        if(album == null)
-            throw new RepException("No album with an ISRC of " + isrc);
-
-        return album;
+    public Album getAlbum(String isrc) throws SQLException {
+        return AlbumDAO.getAlbumDatabase(isrc);
     }
 
-    public boolean addAlbum(Album album) throws RepException {
+    public boolean addAlbum(Album album) throws SQLException, RepException {
         if(album.getIsrc() == null || album.getIsrc().isEmpty() ||
                 album.getTitle() == null || album.getTitle().isEmpty() ||
                 album.getArtist() == null ||
@@ -29,12 +25,15 @@ public class AlbumManagerInDB implements IAlbumManager {
             throw new RepException("Failed to add album: missing required fields");
         else if(album.getReleaseYear() <= 0)
             throw new RepException("Failed to add album: invalid releaseYear");
+        else if(getAlbum(album.getIsrc()) != null)
+            throw new RepException(String.format("Failed to add album: album with an ISRC of %s already exists", album.getIsrc()));
+
 
         return AlbumDAO.insertAlbum(album.getIsrc(), album.getTitle(), album.getContentDesc(),
                 album.getReleaseYear(), album.getArtist().getFirstname(), album.getArtist().getLastname());
     }
 
-    public boolean updateAlbum(Album album) throws RepException {
+    public boolean updateAlbum(Album album) throws SQLException, RepException {
         if(album.getTitle() == null || album.getTitle().isEmpty() ||
                 album.getArtist() == null ||
                 album.getArtist().getFirstname() == null || album.getArtist().getFirstname().isEmpty() ||
@@ -48,7 +47,12 @@ public class AlbumManagerInDB implements IAlbumManager {
                 album.getReleaseYear(), album.getArtist().getFirstname(), album.getArtist().getLastname());
     }
 
-    public boolean deleteAlbum(String isrc) {
-        return AlbumDAO.deleteAlbum(isrc);
+    public boolean deleteAlbum(String isrc) throws SQLException, RepException {
+        boolean success = AlbumDAO.deleteAlbum(isrc);
+
+        if(!success)
+            throw new RepException("Failed to delete album with an ISRC of " + isrc);
+
+        return true; // Always true anyway
     }
 }
