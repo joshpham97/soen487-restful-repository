38230195f3e4b7970c63e95c1b9f2f1fb88e093e
Stefan John Bosco <55599785+TheCriminelz@users.noreply.github.com>
2021-03-16 16:36:24 -0400

    LogFilter fix

--- webapp/src/components/LogFilter.js
+++ webapp/src/components/LogFilter.js
@@ -4,10 +4,16 @@ import {useHistory, useLocation} from "react-router";
 import React from "react";
 import Navbar from "./subcomponents/Navbar";
 import ArrowBackIosRoundedIcon from "@material-ui/icons/ArrowBackIosRounded";
-import {Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import {Button, FormControl, TextField } from "@material-ui/core";
 import { useState, useEffect } from 'react';
 import FilterListIcon from "@material-ui/icons/FilterList";
 import RotateLeftIcon from "@material-ui/icons/RotateLeft";
+
+const changeTypes = [
+    'ADD',
+    'UPDATE',
+    'DELETE'
+];
 
 function LogFilter() {
     const history = useHistory();
@@ -24,38 +30,28 @@ function LogFilter() {
             if(params.filter.changeType)
                 setChangeType(params.filter.changeType);
             if(params.filter.from)
-                setFromDateTime(params.filter.from);
+                setFromDateTime(formatDateTimeForInput(params.filter.from));
             if(params.filter.to)
-                setToDateTime(params.filter.to);
+                setToDateTime(formatDateTimeForInput(params.filter.to));
         }
     }, [location.state]);
 
+    const formatDateTimeForInput = (dateTime) => {
+        return dateTime.substring(0, dateTime.length - 3).replace(' ', 'T');
+    };
+
+    const formatDateTimeForApiCall = (dateTime) => {
+        return dateTime && dateTime !== '' ? dateTime.replace("T", " ") + ':00' : '';
+    };
+
     const getList = () => {
-
-        let modifiedFromDate, modifiedToDate;
-        if(fromDateTime.length !== 0)
-        {
-            modifiedFromDate = fromDateTime.replace("T", " ") + ":00";
-        }
-        else{
-            modifiedFromDate = "";
-        }
-
-        if(toDateTime.length !== 0)
-        {
-            modifiedToDate = toDateTime.replace("T", " ") + ":00";
-        }
-        else{
-            modifiedToDate = "";
-        }
-
         history.push({
             pathname: '/logs',
             state: {
                 filter: {
-                    from: modifiedFromDate,
-                    to: modifiedToDate,
-                    changeType: changeType.toUpperCase()
+                    from: formatDateTimeForApiCall(fromDateTime),
+                    to: formatDateTimeForApiCall(toDateTime),
+                    changeType: changeType
                 }
             }
         });
@@ -75,6 +71,15 @@ function LogFilter() {
         history.push('/logs');
     };
 
+    const renderChangeTypeOptions = () => {
+        return changeTypes.map((c) => {
+            if(c === changeType)
+                return <option key={c} value={c} selected>{c}</option>;
+            else
+                return <option key={c} value={c}>{c}</option>;
+        });
+    };
+
     return (
         <React.Fragment>
             <Navbar />
@@ -88,27 +93,24 @@ function LogFilter() {
 
                 <div className="formRow">
                     <FormControl className="formColumn">
-                        <label htmlFor="logChangeType">ChangeType:</label>
-                        <select id="logChangeType" onChange={(e) => setChangeType(e.currentTarget.value)}>
-                            <option value="">ChangeType</option>
-                            <option value="ADD">ADD</option>
-                            <option value="UPDATE">UPDATE</option>
-                            <option value="DELETE">DELETE</option>
+                        <div>Change Type</div>
+                        <select id="logChangeType" value={changeType}
+                                onChange={(e) => setChangeType(e.currentTarget.value)}>
+                            <option value="">All</option>
+                            {renderChangeTypeOptions()}>
                         </select>
                     </FormControl>
                 </div>
 
                 <div className="formRow">
                     <FormControl className="formColumn">
-                        <label htmlFor="fromDateTime">From:</label>
-                        <input type="datetime-local" id="fromDateTime"
-                               min="2021-03-01T00:00" max="2021-03-31T00:00" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}" onChange={(e) => setFromDateTime(e.currentTarget.value)}/>
+                        <TextField type="datetime-local" label="From" value={fromDateTime} InputLabelProps={{shrink: true}}
+                                   onChange={(e) => setFromDateTime(e.currentTarget.value)}/>
                     </FormControl>
 
                     <FormControl className="formColumn">
-                        <label htmlFor="toDateTime">To:</label>
-                        <input type="datetime-local" id="toDateTime"
-                               min="2021-03-01T00:00" max="2021-03-31T00:00" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}" onChange={(e) => setToDateTime(e.currentTarget.value)}/>
+                        <TextField type="datetime-local" label="To" value={toDateTime} InputLabelProps={{shrink: true}}
+                                   onChange={(e) => setToDateTime(e.currentTarget.value)}/>
                     </FormControl>
                 </div>
 
